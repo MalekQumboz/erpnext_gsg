a0ec4b055548cd911a10e88f9f9571c9f1e69da2
MalekQumboz <malek.hossa.209@gmail.com>
2023-03-11 23:17:51 +0200

    first commit

--- erpnext_gsg/erpnext_gsg/doctype/to_whom_it_concerns/to_whom_it_concerns.py
+++ erpnext_gsg/erpnext_gsg/doctype/to_whom_it_concerns/to_whom_it_concerns.py
@@ -1,8 +1,14 @@
 # Copyright (c) 2023, Malek Qumboz and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class ToWhomItConcerns(Document):
 	pass
+
+	# ما في وقت اكملها للاسف
+	# def validate(self):
+	# 	self.get_salar_slip()
+	# def get_salar_slip(self):
+	# 	salary=frappe.db.sql(f"""select  """)
--- erpnext_gsg/public/js/payment_entry.js
+++ erpnext_gsg/public/js/payment_entry.js
@@ -1,5 +1,6 @@
 frappe.ui.form.on("Payment Entry", {
 	setup: function(frm) {
+//        frm.set_df_property("naming_series", "options", [ "GSG-JV-.YYYY.-"])
         frm.set_df_property("naming_series", "options", [ "GSG-JV-.YYYY.-","ACC-PAY-.YYYY.-"])
     }
 })
